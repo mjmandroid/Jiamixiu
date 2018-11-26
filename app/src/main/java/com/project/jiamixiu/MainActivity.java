7613865d5313e46ff2ceb83aa7c5f062d79b59d7
huangdaran <787346814@qq.com>
2018-11-26 11:10:02 +0800

    test commit

--- app/src/main/java/com/project/jiamixiu/MainActivity.java
+++ app/src/main/java/com/project/jiamixiu/MainActivity.java
@@ -2,7 +2,7 @@ package com.project.jiamixiu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//
 public class MainActivity extends AppCompatActivity {
 
     @Override
