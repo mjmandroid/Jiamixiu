6911239f31ea256b5466a765342dfc89551067d6
huangdaran <787346814@qq.com>
2018-11-28 11:33:55 +0800

    add url

--- app/src/main/java/com/project/jiamixiu/utils/UrlConst.java
+++ app/src/main/java/com/project/jiamixiu/utils/UrlConst.java
@@ -3,4 +3,10 @@ package com.project.jiamixiu.utils;
 public interface UrlConst {
     String baseUrl = "http://jiamixiu.uniondevice.com/";
     String login_url = "api/user/Login";
+    String register_url = "api/user/register";
+    String forget_pwd__url = "api/user/ForgetPwd";
+    String follow_url = "api/video/MyFollows";
+    String fan_url = "api/video/MyFans";
+    String collect_url = "api/video/myFavorite";
+    String del_collect_url = "api/video/FavoriteRemove";
 }
